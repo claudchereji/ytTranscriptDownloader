babd012025d6ed163d3d0a2fba02a50a6541afca
claudchereji <37027984+claudchereji@users.noreply.github.com>
2022-08-28 03:07:22 -0700

    Update copyandpaste.sh

--- copyandpaste.sh
+++ copyandpaste.sh
@@ -1,5 +1,9 @@
 echo "hit ctl,shift, and v to paste the youtube channel url youd like to get transcripts from"
 
+echo -e >
+echo -e >
+echo -e >
+
 read channelurl
 
 yt-dlp $channelurl -i --write-auto-sub --sub-format srt --skip-download && find . -name "*.vtt" -exec python3 vtt2text.py {} \; && rm *.vtt && find . -iname "*.txt" -exec bash -c 'mv "$0" "${0%\.txt}.md"' {} \;
