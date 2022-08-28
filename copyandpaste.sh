0a6c688b3fcdec748ac82e2d9600ae0b8781e3d0
claudchereji <37027984+claudchereji@users.noreply.github.com>
2022-08-28 02:50:53 -0700

    Add files via upload
    bash script to install dependencies and run the download script. run this in the folder that you wanna use to store all the transcripts.

---
+++ copyandpaste.sh
@@ -0,0 +1,6 @@
+echo "hit ctl,shift, and v to paste the youtube channel url youd like to get transcripts from"
+
+read channelurl
+
+yt-dlp $channelurl -i --write-auto-sub --sub-format srt --skip-download && find . -name "*.vtt" -exec python3 vtt2text.py {} \; && rm *.vtt && find . -iname "*.txt" -exec bash -c 'mv "$0" "${0%\.txt}.md"' {} \;
+
